82c9525cb3812276d42c55ab457fdac4a70e9438
manhvd1 <manhvd1@fsoft.com.vn>
2019-03-19 15:19:56 +0700

    up

---
+++ README.md
@@ -0,0 +1,36 @@
+# sandbox
+env sandbox for sales
+
+
+<a href="https://azuredeploy.net/?repository=https://github.com/manhvd/sandbox" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
+
+Click button deploy and waiting.
+
+When process success
+
+![alt text](https://github.com/manhvd/sandbox/blob/master/images/1.PNG)
+
+
+Follow by step: 
+
+![alt text](https://github.com/manhvd/sandbox/blob/master/images/2.PNG)
+
+
+![alt text](https://github.com/manhvd/sandbox/blob/master/images/3.PNG)
+
+
+![alt text](https://github.com/manhvd/sandbox/blob/master/images/4.PNG)
+
+User default: admin, password is: 123456.
+
+
+![alt text](https://github.com/manhvd/sandbox/blob/master/images/5.PNG)
+
+![alt text](https://github.com/manhvd/sandbox/blob/master/images/6.PNG)
+
+IOS:  General Setting > Trust Certificate FPT Software
+
+![alt text](https://github.com/manhvd/sandbox/blob/master/images/IMG_1195.JPG)
+
---
+++ script/start.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -eu
+
+# https://docs.docker.com/engine/installation/linux/ubuntu/#install-using-the-repository
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88 | grep docker@docker.com || exit 1
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo docker run --rm hello-world
+# Make sure your docker engine version is at least version 1.7.1 for docker-compose version 1.5.2
+# Available docker-compose can be seen at https://github.com/docker/compose/releases
+# Change VERSION variable below if you need other version
+VERSION=1.18.0; sudo curl -Lo /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/$VERSION/docker-compose-`uname -s`-`uname -m` && sudo chmod +x /usr/local/bin/docker-compose
+
+sudo apt-get -y install nginx
+
+sudo service nginx stop
+
+git clone git://github.com/manhvd/docker_backend \
+
+cd docker_backend/FE/assets
+ 
+sudo sed -i "s/localhost/$1/g" config.json
+
+cd ../..
+
+cd config
+
+sudo sed -i "s/localhost/$1/g" default.json
+
+cd ..
+ 
+sudo docker-compose -f docker-compose.yml down \
+
+sudo docker-compose -f docker-compose.yml build \
+
+sudo docker-compose -f docker-compose.yml up -d
+
+
